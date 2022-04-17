3da36ce95a4df14c5b73d01ea62ef56d98ab99dd
Florian SAIZONOU <98995493+Rolfy229@users.noreply.github.com>
2022-04-17 15:59:40 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,53 @@
+var document
+
+function showProduct() {
+    document.getElementById("product").style.display = "block"
+}
+function hideProduct() {
+    document.getElementById("product").style.display = "none"
+}
+function showCompany() {
+    document.getElementById("Company").style.display = "block"
+}
+function hideCompany() {
+    document.getElementById("Company").style.display = "none"
+}
+function showConnect() {
+    document.getElementById("Connect").style.display = "block"
+}
+function hideConnect() {
+    document.getElementById("Connect").style.display = "none"
+}
+function rotate1() {
+    document.getElementById("arrow1").style.transform = "rotate(180deg)"
+}
+function norotate1() {
+    document.getElementById("arrow1").style.transform = "rotate(0deg)"
+}
+function rotate2() {
+    document.getElementById("arrow2").style.transform = "rotate(180deg)"
+}
+function norotate2() {
+    document.getElementById("arrow2").style.transform = "rotate(0deg)"
+}
+function rotate3() {
+    document.getElementById("arrow3").style.transform = "rotate(180deg)"
+}
+function norotate3() {
+    document.getElementById("arrow3").style.transform = "rotate(0deg)"
+}
+function show3crox() {
+    document.getElementById("icon-hamburger").style.display = "none"
+    document.getElementById("icon-close").style.display = "block"
+    document.getElementById("NavigationAids").style.display = "block"
+}
+function show3lines() {
+    document.getElementById("icon-hamburger").style.display = "block"
+    document.getElementById("icon-close").style.display = "none"
+    document.getElementById("NavigationAids").style.display = "none"
+}
+
+    
+
+
+
